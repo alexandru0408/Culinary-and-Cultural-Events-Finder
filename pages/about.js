65e47efd1e964708751ad3e89ac8c3729b09255c
alexandru0408 <65963412+alexandru0408@users.noreply.github.com>
2021-08-08 21:09:46 +0200

    Project Setup

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+export default function AboutView() {
+  return (
+    <div>
+      <h1>ABOUT</h1>
+      <p>
+        This is a place where you can search for the latest culinary and other
+        cultural events
+      </p>
+    </div>
+  );
+}
---
+++ pages/events/[slug].js
@@ -0,0 +1,7 @@
+export default function EventView() {
+  return (
+    <div>
+      <h1>Event</h1>
+    </div>
+  );
+}
